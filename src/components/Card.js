8fc3b105c3e30f2e98642c9ee6b6e84b2fe478ad
MarcoSiabatto <siabattorodriguezmarco@gmail.com>
2022-02-13 20:40:02 -0500

    AddedCustomScrollbarsAndAppliedThemToApp

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { BsFillGeoAltFill } from "react-icons/bs";
+import { Scrollbars } from "react-custom-scrollbars";
 
 const Card = (props) => {
   return (
     <div className="card">
-      <img
-        src={props.imageUrl}
-        alt="image-url"
-        className="card--img"
-      />
+      <img src={props.imageUrl} className="card--img" />
       <div className="card--body">
         <div className="card--headers">
           <BsFillGeoAltFill className="card--icon" />
@@ -21,9 +18,13 @@ const Card = (props) => {
           <h2 className="card--title">{props.title}</h2>
           <span className="card--dates">{props.startDate}</span> -&nbsp;
           <span className="card--dates">{props.endDate}</span>
-          <article className="card--description">
-            {props.description}
-          </article>
+          <Scrollbars
+            autoHide
+            autoHideTimeout={2000}
+            style={{ width: "450px" }}
+          >
+            <p className="card--description">{props.description}</p>
+          </Scrollbars>
         </section>
       </div>
     </div>
